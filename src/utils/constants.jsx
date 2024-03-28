868d298d0b8664f37eb397848a2804ef097522b4
manu-hn <manuu.hn11@gmail.com>
2024-03-28 20:12:03 +0530

    useMoviesAndSeries custom hook, redux store, implemented

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,1 +1,13 @@
-export const netflix_logo = 'https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
+export const netflix_logo = 'https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png';
+
+
+export const TMDB_API = `https://api.themoviedb.org/3/movie/now_playing?page=2`;
+
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`
+    }
+};
